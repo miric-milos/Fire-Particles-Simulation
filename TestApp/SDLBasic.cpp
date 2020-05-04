9c59dee408f18fd3fb85ccd1b1fdf6950fb90046
Miloš Mirić <56596844+miric-milos@users.noreply.github.com>
2020-05-04 15:23:50 +0200

    Add project files.

---
+++ TestApp/SDLBasic.cpp
@@ -0,0 +1,52 @@
+#define SDL_MAIN_HANDLED
+
+#include <iostream>
+#include <SDL.h>
+using namespace std;
+
+int main(int argv, char* args[])
+{
+
+	const int SCREEN_WIDTH   = 800;
+	const int SCREEN_HEIGHT  = 600;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		cout << "SDL init failed" << endl;
+		return 1;
+	}
+
+	SDL_Window* window = SDL_CreateWindow("Particle simulation",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+		SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+	if (window == NULL)
+	{
+		// Window failed to create, exit simulation
+		SDL_Quit();
+		return 2;
+	}
+
+	bool quit = false;
+	SDL_Event event;
+
+	while (!quit)
+	{
+		// Update particles
+		// Draw articles
+		// Check for messages/events
+
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+			{
+				quit = true;
+			}
+		}
+	}
+	
+	SDL_DestroyWindow(window); // Destroys window
+	SDL_Quit();
+
+	return 0;
+}
